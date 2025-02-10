d0e4daddc7b4bdf5e79f7b8ea0959df8c1b443b5
Enotta <nvageev_1@edu.hse.ru>
2025-02-10 22:28:46 +0300

    file structure + schemas

---
+++ schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class CityCreate(BaseModel):
+    name: str
+
+class City(BaseModel):
+    id: int
+    name: str
+    latitude: float
+    longitude: float
+
+    class Config:
+        orm_mode = True
